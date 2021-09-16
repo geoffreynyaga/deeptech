1272da2870a6ae90221ea9a60a30d466a969d48f
geoffreynyaga <geoffreynyagak@gmail.com>
2021-09-17 00:04:34 +0300

    AzureStorage

--- custom_storage/custom_azure.py
+++ custom_storage/custom_azure.py
@@ -1,5 +1,11 @@
-class PublicAzureStorage(AzureStorage):
-    account_name = "myaccount"
-    account_key = "mykey"
-    azure_container = "mypublic_container"
-    expiration_secs = None
+from azure.storage.blob import BlockBlobService
+from azure.storage.blob import ContentSettings
+from azure.storage.blob import PublicAccess
+from azure.storage.blob import BlobPermissions
+
+# AzureStorage
+# class PublicAzureStorage(AzureStorage):
+#     account_name = "myaccount"
+#     account_key = "mykey"
+#     azure_container = "mypublic_container"
+#     expiration_secs = None
